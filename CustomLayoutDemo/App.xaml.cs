4f92851edfaf5bf4724fbd02c03097b6c15d9c6b
Tobias Boogh <Tobias Boogh>
2016-08-10 23:10:35 +0200

    Inital init CustomCornerLayout added

---
+++ CustomLayoutDemo/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Unity;
+using CustomLayoutDemo.Views;
+
+namespace CustomLayoutDemo
+{
+    public partial class App : PrismApplication
+    {
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
---
+++ CustomLayoutDemo/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CustomLayoutDemo.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
