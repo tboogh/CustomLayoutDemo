60aa02c19e762e4797dcc5b7b8983510f2a41945
Tobias Boogh <t@boogh.se>
2016-08-15 22:48:25 +0200

    Added ReactiveProperty implementation

--- FormsDemo/ViewModels/ObservableViewModelDemoViewModel.cs
+++ FormsDemo/ViewModels/ObservableViewModelDemoViewModel.cs
@@ -4,17 +4,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
+using Reactive.Bindings;
 using Xamarin.Forms;
 
 namespace FormsDemo.ViewModels
 {
     public class ObservableViewModelDemoViewModel : BindableBase
     {
-        
+        public ReactiveProperty<double> Red { get; }
+        public ReactiveProperty<double> Green { get; }
+        public ReactiveProperty<double> Blue { get; }
+
+        public ReactiveProperty<Color> ResultColor { get; }
 
         public ObservableViewModelDemoViewModel()
         {
-            
+            Red = new ReactiveProperty<double>();
+            Green = new ReactiveProperty<double>();
+            Red = new ReactiveProperty<double>();
+
+            ResultColor = Observable.CombineLatest(Red, Green, Blue, (r, g, b) => Color.FromRgb(r, g, b)).ToReactiveProperty();
         }
     }
 }
